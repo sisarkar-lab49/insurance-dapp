3cef28cb37b24251d9d24b2d9e9edfcab650f71d
Siddhartha Sarkar <siddharth.sarkar@lab49.com>
2022-08-23 11:05:11 +0530

    refactor contract

--- hardhat/contracts/Insurance.sol
+++ hardhat/contracts/Insurance.sol
@@ -3,81 +3,38 @@ pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
 
-import "./Model.sol";
+import "./Payable.sol";
+import "./PriceConsumer.sol";
 
-contract Insurance is Model {
-
-    address private _owner;
-    Policy[] public policies;
-    mapping(address => Subscription[]) subscriptions;
-
-    constructor() payable {
-        _owner = msg.sender;
-        string[] memory features = new string[](3);
-        features[0] = "feature 1";
-        features[1] = "feature 2";
-        features[2] = "feature 3";
-        policies.push(Policy("Platinum", features, "Platinum policy", 5, 25, 200000, 4, "T & C", Status.ACTIVE));
-        policies.push(Policy("Gold", features, "Gold policy", 5, 25, 200000, 4, "T & C", Status.ACTIVE));
-        policies.push(Policy("Silver", features, "Silver policy", 5, 25, 200000, 4, "T & C", Status.ACTIVE));
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Not owner.");
-        _;
-    }
-
-    modifier onlyPolicyHolder() {
-        require(subscriptions[msg.sender].length > 0, "Not a policy holder.");
-        _;
-    }
-
+contract Insurance is Payable, PriceConsumer {
+  
     modifier isValidInput(string calldata _policyName, uint _sumInsured) {
         Policy memory policy = getPolicy(_policyName);
-        require(_sumInsured >= policy.minSumInsured, "selected sum assured is not correct.");
+        require(_sumInsured >= policy.minSumInsured, "sum assured is not correct.");
         _;
     }
 
-    function addPolicy(string calldata _policyName, string[] memory _features, string calldata _description, uint _minTenure, uint _maxTenure, uint _minSumInsured, uint _yeildPercentage, string calldata _terms) external onlyOwner returns (bool){
+    error DuplicatePolicy(string policyName);
+
+    function addPolicy(string calldata _policyName, string[] memory _features,
+     string calldata _description, uint _minTenure, uint _maxTenure, uint _minSumInsured,
+      uint _yeildPercentage, string calldata _terms) external onlyOwner returns (bool){
         if(isPolicyExist(_policyName)) {
-             revert DuplicatePolicy(_policyName);
+            console.log("Policy with name %s already exists.", _policyName);
+            revert DuplicatePolicy(_policyName);
         }
-        policies.push(Policy(_policyName, _features, _description, _minTenure, _maxTenure, _minSumInsured, _yeildPercentage, _terms, Status.ACTIVE));
+        policies.push(Policy(_policyName, _features, _description, _minTenure,
+         _maxTenure, _minSumInsured, _yeildPercentage, _terms, Status.ACTIVE));
+        console.log("Policy added with name %s.", _policyName);
         return true;
-    }
-
-    function isPolicyExist(string calldata _policyName) private view returns (bool){
-        for(uint i=0; i < policies.length; i++){
-            if(keccak256(abi.encodePacked((_policyName))) == keccak256(abi.encodePacked((policies[i].name)))){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function changePolicyStatus(string calldata _policyName, Status _status) external onlyOwner returns (bool){
-        uint policyIndexInArray = getPolicyIndex(_policyName);
-        Policy memory policy = policies[policyIndexInArray];
-        policy.status = _status;
-        policies[policyIndexInArray] = policy;
-        return true;
-    }
-
-     function getPolicyIndex(string calldata _policyName) private view returns (uint){
-        for(uint i=0; i < policies.length; i++){
-            if(keccak256(abi.encodePacked((_policyName))) == keccak256(abi.encodePacked((policies[i].name)))){
-               return i;
-            }
-        }
-        revert PolicyNotFound(_policyName);
     }
 
     function calculatePremium(string calldata _policyName, uint _tenure,
      uint _sumInsured, PremiumFrequancy _premiumFrequency, uint _age)
       public view isValidInput(_policyName, _sumInsured) returns(uint){
         uint basePremium = (_sumInsured * 2)/100;
-
         uint discountOnPremiumFreq = 0;
+
         if(_premiumFrequency == PremiumFrequancy.YEARLY) {
             discountOnPremiumFreq = 2;
         } else if(_premiumFrequency == PremiumFrequancy.HALF_YEARLY) {
@@ -97,77 +54,40 @@ contract Insurance is Model {
         return basePremium - (basePremium*discountOnPremiumFreq)/100 - (basePremium*discountOnAge)/100 - (basePremium*discountOnTenure)/100;
     }
 
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
-
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
-
     function buyPolicy(string calldata _policyName, uint _tenure,
      uint _sumInsured, PremiumFrequancy _premiumFrequency, uint _age)
       payable public isValidInput(_policyName, _sumInsured) returns(bool){
         uint premiumAmount = calculatePremium(_policyName, _tenure, _sumInsured, _premiumFrequency, _age);
+        uint prevBalance = getBalance();
         require(msg.value >= premiumAmount/1000000 ether, "Not enough Ether to buy this policy");
-
+        console.log("transferring %s ether to contact.");
         require(payable(address(this)).send(premiumAmount/1000000 ether));
-
-        Subscription memory subscription = Subscription(_policyName, _tenure, _sumInsured, _premiumFrequency, _age, premiumAmount, premiumAmount, Status.ACTIVE);
+        Subscription memory subscription = Subscription(_policyName, _tenure, _sumInsured,
+         _premiumFrequency, _age, getBalance() - prevBalance, getBalance() - prevBalance, Status.ACTIVE);
         subscriptions[msg.sender].push(subscription);
         return true;
-    }
-
-    function getSubscriptions() external view onlyPolicyHolder returns(Subscription[] memory){
-        return subscriptions[msg.sender];
     }
 
     function claimSettlement(address _policyHolder, uint _subscriptionIndex) public onlyOwner onlyPolicyHolder returns (bool){
         require(isSubscriptionActiveForPolicyHolder(_policyHolder, _subscriptionIndex), "Subscription is not avtive");
 
         Subscription memory subscription = subscriptions[msg.sender][_subscriptionIndex];
-        if(payable(_policyHolder).send(subscription.sumInsured/100000000)){
-            subscriptions[_policyHolder][_subscriptionIndex].status = Status.CLAIMED;
-            return true;
-        }
-        return false;
-    }
+        console.log("transferring %s ether to policy holder.");
+        payable(_policyHolder).transfer(subscription.paidPremium);
 
-    function isSubscriptionActive(uint _subscriptionIndex) external view onlyPolicyHolder returns (bool){
-        require(_subscriptionIndex < subscriptions[msg.sender].length, "Invalid subscription Id.");
-        return isSubscriptionActiveForPolicyHolder(msg.sender, _subscriptionIndex);
-    }
-
-    function isSubscriptionActiveForPolicyHolder(address _policyHolder, uint _subscriptionIndex) private view returns (bool){
-        return (subscriptions[_policyHolder][_subscriptionIndex].status == Status.ACTIVE);
+        subscriptions[_policyHolder][_subscriptionIndex].status = Status.CLAIMED;
+        return true;
     }
 
     function payPremium(uint _subscriptionIndex) payable external returns (bool){
         require(_subscriptionIndex < subscriptions[msg.sender].length, "Invalid subscription Id.");
         Subscription memory subscription = subscriptions[msg.sender][_subscriptionIndex];
-        require(msg.value >= subscription.premiumAmount/100000000,
-         "Not enough Ether to buy this policy");
-        payable(msg.sender).transfer(subscription.premiumAmount/100000000);
-        subscriptions[msg.sender][_subscriptionIndex].paidPremium = subscription.paidPremium + subscription.premiumAmount;
+        uint prevBalance = getBalance();
+        require(msg.value >= subscription.premiumAmount/1000000 ether, "Not enough Ether to pay preminum");
+        console.log("transferring %s ether to contact.");
+        require(payable(address(this)).send(subscription.premiumAmount/1000000 ether));
+        uint premiumPaid = getBalance() - prevBalance;
+        subscriptions[msg.sender][_subscriptionIndex].paidPremium = subscription.paidPremium + premiumPaid;
         return true;
-    }
-
-    function getPolicy(string calldata _policyName) private view returns (Policy memory){
-        for(uint i=0; i < policies.length; i++){
-            if(keccak256(abi.encodePacked((_policyName))) == keccak256(abi.encodePacked((policies[i].name)))){
-               return policies[i];
-            }
-        }
-        revert PolicyNotFound(_policyName);
-    }
-
-    function getAllPolicies() external view returns(Policy[] memory _policies){
-        _policies = policies;
-    }
-
-    function getBalance() public view onlyOwner returns (uint) {
-        return address(this).balance;
-    }
-
-    function withdraw() external onlyOwner {
-        payable(_owner).transfer(getBalance());
     }
 }
